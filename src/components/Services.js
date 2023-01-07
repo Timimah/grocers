0b6706efe6994fe59a7e4aee5dbe1ef974bd855a
Timimah <44163985+Timimah@users.noreply.github.com>
2023-01-07 18:35:51 +0100

    added Services component

---
+++ src/components/Services.js
@@ -0,0 +1,44 @@
+
+const Services = () => {
+  return (
+    <>
+        <div className="bg-amber-600/50 h-screen w-3/4 px-10 pt-5 my-3 text-center font-extralight mx-auto rounded-md">
+            <div className="font-light text-3xl">Our Services</div>
+            <div className="my-5">
+                <p className="flex justify-center text-xl">
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                    Fast Delivery
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                </p>
+                <p className="px-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia at, suscipit ex cum nobis sed iste amet eius architecto quas sit repellat aperiam maiores distinctio laboriosam eos, hic ea officia.</p>
+            </div>
+            <div className="my-5">
+                <p className="flex justify-center text-xl">
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                    Fast Delivery
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                </p>
+                <p className="px-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia at, suscipit ex cum nobis sed iste amet eius architecto quas sit repellat aperiam maiores distinctio laboriosam eos, hic ea officia.</p>
+            </div>
+            <div className="my-5">
+                <p className="flex justify-center text-xl">
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                    Fast Delivery
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                </p>
+                <p className="px-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia at, suscipit ex cum nobis sed iste amet eius architecto quas sit repellat aperiam maiores distinctio laboriosam eos, hic ea officia.</p>
+            </div>
+            <div className="my-5">
+                <p className="flex justify-center text-xl">
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                    Fast Delivery
+                    <span className="my-auto mx-2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="burlywood" className="w-5 h-5"><path d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z" /></svg></span>
+                </p>
+                <p className="px-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia at, suscipit ex cum nobis sed iste amet eius architecto quas sit repellat aperiam maiores distinctio laboriosam eos, hic ea officia.</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Services
