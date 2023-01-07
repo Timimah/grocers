c63a064daf155bff984893e941309d95d873187c
Timimah <44163985+Timimah@users.noreply.github.com>
2023-01-07 18:36:22 +0100

    added Landing Component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,16 @@
-import pickle from '../images/pickle.jpg'
+import { Link } from "react-router-dom"
 
 const Landing = () => {
     return(
         <>
-            <img className='-z-10 absolute w-full h-full' src={pickle} alt="" srcset="" />
-            <div className="h-screen text-center text-6xl font-medium text-white mt-48">
-                <h1>Pickle Perfection</h1>
-                <button className='text-2xl bg-amber-600/50 px-5 py-3 mt-10'>Shop Now</button>
+            <div className="bg-pickle bg-no-repeat bg-cover">
+                <div className="h-screen text-center text-6xl font-medium text-white p-48 bg-amber-600/50">
+                    <h1>Shopping has never been easier!</h1>
+                    <Link to="/shop" className='text-2xl bg-amber-600/50 px-5 py-3 mt-10'>Shop Now!</Link>
+                </div>
             </div>
         </>
     )
 }
 
-export default Landing
+export default Landing      
